07b3c28a602182496515e921c6410c9b678fe059
quachang <qchang96@gmail.com>
2020-04-15 21:14:20 -0400

    first commit

---
+++ src/com/techcircle/java/javaBasic/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.techcircle.java.javaBasic;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+//		System.out.println("Hello Techcircle!!");
+		
+		
+		String color = "Black";
+		
+		System.out.println(color);
+		
+		String type = "Hyundai";
+		
+		System.out.println(type);
+		
+		
+	}
+}
